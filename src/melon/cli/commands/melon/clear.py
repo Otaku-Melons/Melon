023f426c17b84dfddc473ead5f3f4f2d66034462
DUB1401 <vlad.milosta@yandex.ru>
2026-08-13 18:55:51 +0300

    Add: clear broken files

--- src/melon/cli/commands/melon/clear.py
+++ src/melon/cli/commands/melon/clear.py
@@ -1,9 +1,11 @@
+import os
 from dataclasses import dataclass
 from enum import Enum
+from json import JSONDecodeError
 from pathlib import Path
 
 from dublib.cli.terminalyzer import Command, ParsedCommandData
-from dublib.functions.filesystem import RemoveDirectoryContent
+from dublib.functions.filesystem import ReadJSON, RemoveDirectoryContent
 
 from .... import utils
 from ..base_processor import PreparedData, T_SingleParserRequired
@@ -17,6 +19,7 @@ class ClearingRules(Enum):
 	"""Правила очистки."""
 
 	All = "-all"
+	Broken = "-broken"
 	NotFound = "-not-found"
 
 @dataclass(frozen = True)
@@ -60,6 +63,114 @@ class CommandProcessor(CommandProcessorTemplate[Parameters]):
 		return FilesRemoved
 
 	#==========================================================================================#
+	# >>>>> ПРИВАТНЫЕ МЕТОДЫ РЕАЛИЗАЦИИ ПРАВИЛ <<<<< #
+	#==========================================================================================#
+
+	def __ClearAll(self, parameters: Parameters) -> bool:
+		"""
+		Реализует стратегию очистки: удалить всё.
+
+		:param parameters: Параметры команды.
+		:type parameters: Parameters
+		:return: Возвращает `False`, если команда требует прерывания выполнения.
+		:rtype: bool
+		"""
+
+		ParserSettings = parameters.required_parser.settings
+		RemoveDirectoryContent(ParserSettings.directories.titles)
+		self.printer.emit("All files removed.")
+
+		return True
+
+	def __ClearBroken(self, parameters: Parameters) -> bool:
+		"""
+		Реализует стратегию очистки: повреждённый файлы.
+
+		:param parameters: Параметры команды.
+		:type parameters: Parameters
+		:return: Возвращает `False`, если команда требует прерывания выполнения.
+		:rtype: bool
+		"""
+		
+		SourceOperator = parameters.required_parser.source_operator
+		ParserSettings = parameters.required_parser.settings
+		TitlesDirectoryPath = ParserSettings.directories.titles
+		ProgressIndicator = SourceOperator.portals.printer.progress_indicator
+
+		Files = tuple(Entry.name for Entry in os.scandir(TitlesDirectoryPath) if Entry.is_file() and Entry.name.endswith(".json"))
+		FilesCount: int = len(Files)
+		FileIndex: int = 1
+		FilesToRemove: list[str] = []
+
+		for File in Files:
+			Progress = (FileIndex + 1) / FilesCount * 100.0
+			ProgressIndicator.set_progress(Progress)
+
+			try:
+				ReadJSON(TitlesDirectoryPath / File)
+			except JSONDecodeError:
+				self.printer.emit(f"File <i>{File}</i> marked for removing.")
+				FilesToRemove.append(File)
+
+			FileIndex += 1
+
+		ProgressIndicator.end()
+		RemovedFilesCount: int = self.__RemoveFilesInDirectory(TitlesDirectoryPath, FilesToRemove)
+		if RemovedFilesCount: self.printer.emit(f"Removed {RemovedFilesCount} files.")
+		else: self.printer.emit("No broken files found.")
+		
+		return True
+
+	def __ClearNotFound(self, parameters: Parameters) -> bool:
+		"""
+		Реализует стратегию очистки: файлы, описывающие тайтлы, не найденные по алиасу на сервере.
+
+		:param parameters: Параметры команды.
+		:type parameters: Parameters
+		:return: Возвращает `False`, если команда требует прерывания выполнения.
+		:rtype: bool
+		"""
+		
+		SourceOperator = parameters.required_parser.source_operator
+		ParserSettings = parameters.required_parser.settings
+		ProgressIndicator = SourceOperator.portals.printer.progress_indicator
+
+		Collector = utils.Collector(SourceOperator)
+		self.printer.emit("Scanning local titles… ", end_line = False, flush = True)
+		LocalTitles: dict[str, str] = Collector.scan_local()
+		self.printer.emit("Done.")
+		SlugsCount: int = len(LocalTitles)
+		self.printer.emit(f"Local titles found: {SlugsCount}.")
+		
+		SlugIndex: int = 0
+		FilesToRemove: list[str] = []
+
+		for Slug in Collector.slugs:
+			Progress = (SlugIndex + 1) / SlugsCount * 100.0
+			ProgressIndicator.set_progress(Progress)
+
+			IsTitleExists: bool | None = SourceOperator.is_title_exists(Slug)
+
+			if IsTitleExists is None:
+				if SlugIndex: self.printer.error("Error occurred during the existence checking.")
+				else: self.printer.error("Parser doesn't provide existence checker method.")
+				return False
+
+			if IsTitleExists is False:
+				File = LocalTitles[Slug]
+				self.printer.emit(f"File <i>{File}</i> marked for removing.")
+				FilesToRemove.append(File)
+
+			SlugIndex += 1
+			if SlugIndex != SlugsCount: ParserSettings.common.sleep_delay()
+
+		ProgressIndicator.end()
+		FilesRemoved: int = self.__RemoveFilesInDirectory(ParserSettings.directories.titles, FilesToRemove)
+		self.printer.emit(f"Files removed: {FilesRemoved}.")
+
+		return True
+
+	#==========================================================================================#
 	# >>>>> ПЕРЕОПРЕДЕЛЯЕМЫЕ МЕТОДЫ <<<<< #
 	#==========================================================================================#
 
@@ -87,7 +198,8 @@ class CommandProcessor(CommandProcessorTemplate[Parameters]):
 
 		ComPos = command.create_position(name = "RULE", description = "Rule to clearing files.", important = True)
 		ComPos.add_flag(ClearingRules.All.value, description = "Delete all files.")
-		ComPos.add_flag(ClearingRules.NotFound.value, description = "Clear titles, that not found on server.")
+		ComPos.add_flag(ClearingRules.Broken.value, description = "Delete broken JSON files.")
+		ComPos.add_flag(ClearingRules.NotFound.value, description = "Delete files for titles that not found on server by slug.")
 
 		self._AddMirrorKey()
 
@@ -122,49 +234,11 @@ class CommandProcessor(CommandProcessorTemplate[Parameters]):
 		:rtype: bool
 		"""
 
-		SourceOperator = parameters.required_parser.source_operator
-		ParserSettings = parameters.required_parser.settings
-		ProgressIndicator = SourceOperator.portals.printer.progress_indicator
+		Result: bool = True
 
 		match parameters.rule:
+			case ClearingRules.All: Result = self.__ClearAll(parameters)
+			case ClearingRules.Broken: Result = self.__ClearBroken(parameters)
+			case ClearingRules.NotFound: Result = self.__ClearNotFound(parameters)
 
-			case ClearingRules.All:
-				RemoveDirectoryContent(ParserSettings.directories.titles)
-				self.printer.emit("All files removed.")
-
-			case ClearingRules.NotFound:
-				Collector = utils.Collector(SourceOperator)
-				self.printer.emit("Scanning local titles… ", end_line = False, flush = True)
-				LocalTitles: dict[str, str] = Collector.scan_local()
-				self.printer.emit("Done.")
-				SlugsCount: int = len(LocalTitles)
-				self.printer.emit(f"Local titles found: {SlugsCount}.")
-				
-				SlugIndex: int = 0
-				FilesToRemove: list[str] = []
-				Progress: float = 0.0
-
-				for Slug in Collector.slugs:
-					Progress = (SlugIndex + 1) / SlugsCount * 100.0
-					ProgressIndicator.set_progress(Progress)
-
-					IsTitleExists: bool | None = SourceOperator.is_title_exists(Slug)
-
-					if IsTitleExists is None:
-						if SlugIndex: self.printer.error("Error occurred during the existence checking.")
-						else: self.printer.error("Parser doesn't provide existence checker method.")
-						return False
-
-					if IsTitleExists is False:
-						File = LocalTitles[Slug]
-						self.printer.emit(f"File <i>{File}</i> marked for removing.")
-						FilesToRemove.append(File)
-
-					SlugIndex += 1
-					if SlugIndex != SlugsCount: ParserSettings.common.sleep_delay()
-
-				ProgressIndicator.end()
-				FilesRemoved: int = self.__RemoveFilesInDirectory(ParserSettings.directories.titles, FilesToRemove)
-				self.printer.emit(f"Files removed: {FilesRemoved}.")
-
-		return True
+		return Result
